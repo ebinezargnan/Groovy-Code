409c62bf3de57225c3d70db829b2e2fe3d152cc0
ebinezargnan <gnanasekaran.e@gmail.com>
2020-09-12 12:01:07 +0530

    first commit

---
+++ src/resources/groovy/Utils.groovy
@@ -0,0 +1,8 @@
+
+
+  def validateString(String inputField) {
+      if (!inputField ?.trim()) {
+          println "The value you entered is -${inputField}. Enter Valid Input of not null and not empty."
+          return
+      }
+  }
